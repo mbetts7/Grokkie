9e1208a8556ff54c5b0f72f0973e5d6432a895d6
mbetts7 <mbetts7@gmail.com>
2014-02-21 17:28:41 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Grokkie::Application.config.secret_key_base = 'edce92ea9e8c4b938451359a4cbd7b2ca0223ed308ccb60586973e9a3bd5d51dbbb920905b5e9c859bbc54d73cb59bb83a805477afb8ded0d8e79066f90cd415'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Grokkie::Application.config.session_store :cookie_store, key: '_grokkie_session'
