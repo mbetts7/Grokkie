1a2b4c939c40dc984df844283f0de04a9754456c
kevinrogerteng <kevin.roger.teng@gmail.com>
2014-02-21 22:15:48 -0800

    made adjustments to comments model to include polymorphic

--- app/models/roadmap.rb
+++ app/models/roadmap.rb
@@ -1,2 +1,5 @@
 class Roadmap < ActiveRecord::Base
+	has_many :resources
+	has_many :comments, as: :commentable, dependent: :destroy
+	# belongs_to :user
 end
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,3 +1,4 @@
 class Skill < ActiveRecord::Base
   belongs_to :category
+  has_many :roadmaps
 end
