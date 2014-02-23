827fc7b71212bc16a74a4ecf2ad13f5eedeaf734
kevinrogerteng <kevin.roger.teng@gmail.com>
2014-02-23 15:31:56 -0800

    added a username column to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,13 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
   has_many :roadmaps
+
+  def self.from_omniauth(auth)
+  	where(auth.slic(:provider, :uid)).first_or_create do |user|
+  		user.provider = auth.provider
+  		user.uid = auth.uid
+  		user.username = auth.info.nickname
+  	end
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140223223605) do
+ActiveRecord::Schema.define(version: 20140223233123) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -78,6 +78,9 @@ ActiveRecord::Schema.define(version: 20140223223605) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
