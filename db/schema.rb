dc635dd8350d11b6454db05221a09d255c99556f
kevinrogerteng <kevin.roger.teng@gmail.com>
2014-02-24 15:21:31 -0800

    users profile picture now added to model when signing up with linkedin for the first time

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140224222908) do
+ActiveRecord::Schema.define(version: 20140224225342) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -89,6 +89,7 @@ ActiveRecord::Schema.define(version: 20140224222908) do
     t.string   "github_name"
     t.text     "bio"
     t.string   "learning_style"
+    t.string   "image"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
