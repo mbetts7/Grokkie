da32ee886a2a927059b0440c8a9194b53827b3bf
mbetts7 <mbetts7@gmail.com>
2014-02-24 15:29:31 -0800

    added migration for resources

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -2,15 +2,19 @@
 #
 # Table name: resources
 #
-#  id          :integer          not null, primary key
-#  name        :text
-#  url         :string(255)
-#  description :string(255)
-#  position_id :integer
-#  roadmap_id  :integer
-#  created_at  :datetime
-#  updated_at  :datetime
-#  image_url   :text
+#  id                      :integer          not null, primary key
+#  name                    :text
+#  url                     :string(255)
+#  description             :string(255)
+#  position_id             :integer
+#  roadmap_id              :integer
+#  created_at              :datetime
+#  updated_at              :datetime
+#  image_url               :text
+#  attachment_file_name    :string(255)
+#  attachment_content_type :string(255)
+#  attachment_file_size    :integer
+#  attachment_updated_at   :datetime
 #
 
 class Resource < ActiveRecord::Base
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140224192847) do
+ActiveRecord::Schema.define(version: 20140224223348) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,10 @@ ActiveRecord::Schema.define(version: 20140224192847) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text     "image_url"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "comments", force: true do |t|
@@ -43,6 +47,10 @@ ActiveRecord::Schema.define(version: 20140224192847) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text     "image_url"
+    t.string   "attachment_file_name"
+    t.string   "attachment_content_type"
+    t.integer  "attachment_file_size"
+    t.datetime "attachment_updated_at"
   end
 
   add_index "resources", ["roadmap_id"], name: "index_resources_on_roadmap_id", using: :btree
