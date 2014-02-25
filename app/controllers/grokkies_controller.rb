a84369afed03b7a8aa19af9ac078d2bcdbab4aea
afleisch <afleisch@gmail.com>
2014-02-25 09:05:06 -0800

    removed unneeded varaible and logic from homepage controller

--- app/controllers/grokkies_controller.rb
+++ app/controllers/grokkies_controller.rb
@@ -3,14 +3,7 @@ class GrokkiesController < ApplicationController
   	@categories = Category.all
    
     roadmaps = Roadmap.all
-    roadmap_max = roadmaps.length
     @random_roadmaps = Roadmap.all.sample(6)
-    # 36.times do
-    #   roadmap = Roadmap.find(rand(1..roadmap_max))
-    #   (@random_roadmaps << roadmap).uniq
-      
-
-    # end
     
   end
 end
