64b6fdc09a0676ce6ba2371e89226b28a5a27a59
kevinrogerteng <kevin.roger.teng@gmail.com>
2014-02-25 10:45:10 -0800

    added a hidden field for position_id in new resource form

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,11 +1,14 @@
 class ResourcesController < ApplicationController
   def new
+    roadmap = Roadmap.find(params[:roadmap_id])
+    @new_position = roadmap.resources.length + 1
     @resource = Resource.new()
   end
 
   def create
     roadmap = Roadmap.find(params[:roadmap_id])
     new_resource = roadmap.resources.create(resource_params)
+    binding.pry
     redirect_to roadmap_resource_path(roadmap, new_resource)
   end
 
@@ -33,6 +36,6 @@ class ResourcesController < ApplicationController
   private
 
   def resource_params
-    params.require(:resource).permit(:name, :url, :description)
+    params.require(:resource).permit(:name, :url, :description, :position_id)
   end
 end
