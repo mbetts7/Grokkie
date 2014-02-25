d826054255e682824db72862ed66b2e20400dbea
afleisch <afleisch@gmail.com>
2014-02-24 22:21:22 -0800

    refactored roadmap randomizing

--- app/controllers/grokkies_controller.rb
+++ app/controllers/grokkies_controller.rb
@@ -2,20 +2,13 @@ class GrokkiesController < ApplicationController
   def index
   	@categories = Category.all
    
-
-    # random_categories.each do |c|
-    #   @random_skill = c.skills.first
-    # end
-    # @random_roadmap = @random_skill.roadmaps.first
-    cat_maxnum = Category.all.length
-    category = Category.find(rand(1..cat_maxnum))
-    roadmaps = category.roadmaps
-    roadmap_maxnum= roadmaps.length
-    @random_roadmap_1 = roadmaps[rand(0..roadmap_maxnum)]
-    @random_roadmap_2 = roadmaps[rand(0..roadmap_maxnum)]
-    @random_roadmap_3 = roadmaps[rand(0..roadmap_maxnum)]
-    @random_roadmap_4 = roadmaps[rand(0..roadmap_maxnum)]
-    @random_roadmap_5 = roadmaps[rand(0..roadmap_maxnum)]
-    @random_roadmap_6 = roadmaps[rand(0..roadmap_maxnum)]
+    roadmaps = Roadmap.all
+    roadmap_max = roadmaps.length
+    @random_roadmaps = []
+    6.times do
+      roadmap = Roadmap.find(rand(1..roadmap_max))
+      @random_roadmaps << roadmap
+    end
+    
   end
 end
