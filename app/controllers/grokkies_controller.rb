3596c6cf310d8270b567245d798afba957048212
afleisch <afleisch@gmail.com>
2014-02-24 21:22:03 -0800

    first commit

--- app/controllers/grokkies_controller.rb
+++ app/controllers/grokkies_controller.rb
@@ -1,5 +1,21 @@
 class GrokkiesController < ApplicationController
   def index
   	@categories = Category.all
+   
+
+    # random_categories.each do |c|
+    #   @random_skill = c.skills.first
+    # end
+    # @random_roadmap = @random_skill.roadmaps.first
+    cat_maxnum = Category.all.length
+    category = Category.find(rand(1..cat_maxnum))
+    roadmaps = category.roadmaps
+    roadmap_maxnum= roadmaps.length
+    @random_roadmap_1 = roadmaps[rand(0..roadmap_maxnum)]
+    @random_roadmap_2 = roadmaps[rand(0..roadmap_maxnum)]
+    @random_roadmap_3 = roadmaps[rand(0..roadmap_maxnum)]
+    @random_roadmap_4 = roadmaps[rand(0..roadmap_maxnum)]
+    @random_roadmap_5 = roadmaps[rand(0..roadmap_maxnum)]
+    @random_roadmap_6 = roadmaps[rand(0..roadmap_maxnum)]
   end
 end
