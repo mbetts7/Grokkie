eba24a2dea5a9d7ece052dd7514c227ae5b735c8
kevinrogerteng <kevin.roger.teng@gmail.com>
2014-02-26 12:45:11 -0800

    added check for if user signed in on roadmap and resource show page

--- app/helpers/roadmaps_helper.rb
+++ app/helpers/roadmaps_helper.rb
@@ -1,6 +1,7 @@
 module RoadmapsHelper
 
 	def check_owner (current_user, roadmap)
+		binding.pry
 		id = []
 		user_roadmaps = current_user.roadmaps
 		user_roadmaps.each {|roadmap| id << roadmap.id}
