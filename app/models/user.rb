5e331f2610a12e1dfa6edb2dc517500bf2e929ba
mbetts7 <mbetts7@gmail.com>
2014-02-26 14:01:49 -0800

    displaying default profile photo if using devise and no profile uploaded, also fixed some css and changed user 1 seed

--- app/models/user.rb
+++ app/models/user.rb
@@ -44,7 +44,7 @@ class User < ActiveRecord::Base
       thumb: '100x100>',
       square: '200x200#',
       medium: '300x300>', 
-      }
+      }, default_url: 'default_avatar.gif'
     
   validates_attachment_content_type :avatar, content_type: /\Aimage/, message: 'file type not allowed, please only upload images'
   # paperclip #
