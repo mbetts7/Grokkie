6297ce5b6c14061dbbc61e59ce6c2466bc6b9611
kevinrogerteng <kevin.roger.teng@gmail.com>
2014-02-26 11:30:38 -0800

    created a helper method for ownership validation

--- app/helpers/roadmaps_helper.rb
+++ app/helpers/roadmaps_helper.rb
@@ -1,2 +1,10 @@
 module RoadmapsHelper
+
+	def check_owner (current_user, roadmap)
+		id = []
+		user_roadmaps = current_user.roadmaps
+		user_roadmaps.each {|roadmap| id << roadmap.id}
+		binding.pry
+		id.include?(roadmap.id)
+	end
 end
