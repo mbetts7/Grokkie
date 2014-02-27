ca3b2baffc15b4f6d82005fff1287df2af63fa25
afleisch <afleisch@gmail.com>
2014-02-27 10:28:37 -0800

    added users to seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,11 @@ users << User.create(username: 'Finn789', email: 'FinnMurray@rhyta.com', passwor
 users << User.create(username: 'Ashton87', email: 'AshtonRide@jourrapide.com', password: 'password', password_confirmation: 'password')
 users << User.create(username: 'Zac0987', email: 'ZacChave@rhyta.com', password: 'password', password_confirmation: 'password')
 users << User.create(username: 'Ruby098', email: 'RubySpain@jourrapide.com', password: 'password', password_confirmation: 'password')
-
+users << User.create(username: 'Bob39', email: 'bob39@rhyta.com', password: 'password', password_confirmation: 'password')
+users << User.create(username: 'Dave123', email: 'dave123@rhyta.com', password: 'password', password_confirmation: 'password')
+users << User.create(username: 'Cal2014', email: 'cal2014@rhyta.com', password: 'password', password_confirmation: 'password')
+users << User.create(username: 'bkyln112', email: 'bklyn112@rhyta.com', password: 'password', password_confirmation: 'password')
+users << User.create(username: 'george1', email: 'george1@rhyta.com', password: 'password', password_confirmation: 'password')
 # users << User.create!(email: 'mbetts7@gmail.com', password: 'password', password_confirmation: 'password')
 # users << User.create!(email: 'FinnMurray@rhyta.com', password: 'password', password_confirmation: 'password')
 # users << User.create!(email: 'AshtonRide@jourrapide.com', password: 'password', password_confirmation: 'password')
