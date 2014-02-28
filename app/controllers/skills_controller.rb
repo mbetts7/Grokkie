eee212ea66b8061ae536a5f954bbc03db97b42c3
mbetts7 <mbetts7@gmail.com>
2014-02-27 16:30:49 -0800

    fixed up skills page a little bit

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -2,7 +2,7 @@ class SkillsController < ApplicationController
   def show
   	category_id = params[:category_id]
   	skill_id = params[:id]
-  	category = Category.find(category_id)
-  	@skill = category.skills.find(skill_id)
+  	@category = Category.find(category_id)
+  	@skill = @category.skills.find(skill_id)
   end
 end
